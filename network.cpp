59076bfe8ed7d5bbdc08390337df36e8bedbc93d
Wille Kantelus <wille.kantelus@tuni.fi>
2020-02-05 13:05:15 +0200

    refactored network.cpp

--- network.cpp
+++ network.cpp
@@ -8,24 +8,31 @@ Network::Network(QObject *parent) : QObject(parent)
 void Network::postRequest(QString url)
 {
     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
-    /*
-    connect(manager, &QNetworkAccessManager::finished,
-            this, &Network::replyFinished);
-    */
     QNetworkRequest request(url);
     request.setHeader(QNetworkRequest::ContentTypeHeader,
         "application/x-www-form-urlencoded");
     //QUrl postData;
+
+    // Tähän saa form-dataa...
+    // täytyy deletoida jossain vaiheessa! https://doc.qt.io/archives/qt-4.8/qhttpmultipart.html
     QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
 
+    // ... näin ->
+    /*
     QHttpPart textPart;
     textPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"dnn$ctr1025$Etusivu$txtHakuEtunimi2\""));
     textPart.setBody("Wille");
 
+    multipart->append(textPart);
+    */
+
     connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
     //manager->get(QNetworkRequest(QUrl(url)));
     //manager->post(request, postData.query().toUtf8());
-    manager->post(request, multiPart);
+   QNetworkReply* reply = manager->post(request, multiPart);
+
+   // nyt multipart poistuu kun reply poistuu :)
+   multiPart->setParent(reply);
 }
 
 void Network::replyFinished(QNetworkReply *reply)
